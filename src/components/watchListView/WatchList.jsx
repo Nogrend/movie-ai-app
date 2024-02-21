c599a071e75612cd56ad1ee8deb8937fc5f79dd6
bram <nogrend@msn.com>
2024-02-21 16:15:56 +0100

    get movie ids from api

--- src/components/watchListView/WatchList.jsx
+++ src/components/watchListView/WatchList.jsx
@@ -1,23 +1,31 @@
-import {getMovieById} from "./queries.js";
+import {getMovieById, getWatchListById} from "./queries.js";
 import {useQuery} from "@tanstack/react-query";
 import MovieView from "../movieView/MovieView.jsx";
 import "./WatchList.css"
 
-
 const WatchList = () => {
 
-    const movieIds = ['tt7286456', 'tt5001754', 'tt0371746', 'tt27687527'];
+    const watchListId = "f9051bf2-c865-487a-854b-25c587688e68";
 
-    const {data} = useQuery({
-        queryKey: ['data'],
+    const {data: movieIds} = useQuery({
+        queryKey: ['movieIds'],
+        queryFn: () => {
+            return getWatchListById(watchListId);
+        },
+        select: (data) => data.data["movieIds"]
+    });
+
+    const {data: movieDetails} = useQuery({
+        queryKey: ['movieDetails'],
         queryFn: async () => {
             const moviePromises = movieIds.map(id => getMovieById(id));
             const movies = await Promise.all(moviePromises);
             return movies.map((movie) => movie.data)
-        }
+        },
+        enabled: !!movieIds
     });
 
-    return data && <MovieView movies={data}/>
+    return movieDetails && <MovieView movies={movieDetails}/>
 }
 
 export default WatchList;
