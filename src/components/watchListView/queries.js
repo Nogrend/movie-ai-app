38a4ffd86223069ca12424795a6419f2bfc0a354
bram <nogrend@msn.com>
2024-02-21 16:13:41 +0100

    add getWatchListById query

--- src/components/watchListView/queries.js
+++ src/components/watchListView/queries.js
@@ -1,7 +1,12 @@
 import apiOpenMovieDatabase from "../../config/apiOpenMovieDatabase.js";
+import apiWatchList from "../../config/apiWatchList.js";
 
 export const getMovieById = async (id) => {
     return apiOpenMovieDatabase.get(`/?i=${id}&apikey=${import.meta.env.VITE_API_KEY}`);
+}
+
+export const getWatchListById = (id) => {
+    return apiWatchList.get(`/watchlist/${id}`);
 }
 
 
